182ba74e101dc68a6f9470edba390ad2360f432e
sharanjan <sharanjan18@gmail.com>
2021-10-03 21:09:47 +0530

    Login componet file

--- frontend/imageUploader/src/app/login/login.component.ts
+++ frontend/imageUploader/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router, ActivatedRoute } from "@angular/router";
 import { compilePipeFromMetadata } from '@angular/compiler';
 
 
@@ -11,11 +13,20 @@ import { compilePipeFromMetadata } from '@angular/compiler';
 })
 export class LoginComponent implements OnInit {
 
-  
+  url;
 
+  constructor(private http: HttpClient, private route: ActivatedRoute,
+    private router: Router,){}
 
   ngOnInit(): void {
-   
+    this.http.get<any>('http://localhost:5550/').subscribe(
+      data=>{
+        console.log(data);
+
+        this.url = data;
+        
+      },
+      error=>console.log(error));
   }
 
 }
